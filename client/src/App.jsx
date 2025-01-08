16469a11c5dd93504387b7fbbbf969b38c8f858b
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-01-08 11:16:07 +0700

    version 1

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl">World</h1>
+    </>
+  );
+}
+
+export default App;
