bcf4b57c42f5748cc1a3f096d34f56aee9c63daa
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-01-08 21:39:48 +0700

    connect to DB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connectDB.js";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
