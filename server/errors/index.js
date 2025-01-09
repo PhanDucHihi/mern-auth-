91a0324c9951450d794ff7caed3c2c154ff04c4b
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-01-09 11:45:51 +0700

    Create Error Custom

---
+++ server/errors/index.js
@@ -0,0 +1,6 @@
+import CustomAPIError from "./custom_api.js";
+import BadRequestError from "./badRequest.js";
+import NotFoundError from "./notFound.js";
+import Unauthenticated from "./unauthenticated.js";
+
+export { CustomAPIError, NotFoundError, BadRequestError, Unauthenticated };
